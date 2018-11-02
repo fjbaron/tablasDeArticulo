fdf9724a8f40ceadd77df5ab2bad6b00ef1dee2f
Javier Baron <fjbaron@gmail.com>
2018-11-02 10:08:03 +0100

    Cambios de los ultimos meses

--- R/tablasDeArticulo.R
+++ R/tablasDeArticulo.R
@@ -50,6 +50,7 @@ generaTablaDescriptivaNumericasPorTiempo=function(df,TiempoFactor,vNumericas,tra
 
 
 
+
 generaTablatTestPorGrupo=function(df,vGrupo,vNumericas,traduccion,columnas=c("n","mediaet")){
   names(traduccion)=vNumericas
   tablaRes=Reduce(rbind,
@@ -62,7 +63,7 @@ generaTablatTestPorGrupo=function(df,vGrupo,vNumericas,traduccion,columnas=c("n"
                         res
                       }
                     ),#AQUI VA LA LISTA t-test
-                    list(descTtest(df,vNum,vGrupo)[,c("p.t","ci95")])
+                    list(descTtest(df,vNum,vGrupo)[,c("t","p.t","ci95", "W","p.w")])
                     )#Aquí termina t-test
                     )
                     %>% mutate(Variable=traduccion[vNum]) %>%
@@ -71,6 +72,11 @@ generaTablatTestPorGrupo=function(df,vGrupo,vNumericas,traduccion,columnas=c("n"
   )
   tablaRes
 }
+
+
+
+
+
 
 
 generaTablaANOVA1F=function(df,vGrupo,vNumericas,traduccion,columnas=c("n","mediaet")){
@@ -115,17 +121,44 @@ generaTablatTestPorTiempoGrupo=function(df,TiempoFactor,vGrupo,vNumericas,traduc
                             res
                           }
                         ),#AQUI VA LA LISTA t-test
-                        list(descTtest(dfTiempo,vNum,vGrupo)[,c("p.t","ci95")])
+                        list(descTtest(dfTiempo,vNum,vGrupo)[,c("t","p.t","ci95", "W","p.w")])
                         )#Aquí termina t-test
                         )
                     ))%>% mutate(Variable=traduccion[vNum],
-                                 Tiempo=levels(dfLong[,TiempoFactor])) %>% select(Variable,Tiempo,everything())
+                                 Tiempo=levels(df[,TiempoFactor])) %>% select(Variable,Tiempo,everything())
                   )
   )
   tablaRes
 }
 
 
+
+
+
+generaTablaANOVA1FPorTiempo=function(df,TiempoFactor,vGrupo,vNumericas,traduccion,columnas=c("n","mediaet")){
+  names(traduccion)=vNumericas
+  tablaRes=Reduce(rbind,
+                  lapply(vNumericas,FUN=function(vNum)
+                    Reduce(rbind, lapply(
+                      split(df,df[,TiempoFactor]),
+                      FUN = function(dfTiempo)
+                        Reduce(cbind, append(lapply(
+                          split(dfTiempo,dfTiempo[,vGrupo]),
+                          FUN = function(dfGr){
+                            res=desc1vn(dfGr, vNum)[, columnas]
+                            names(res)=sprintf("%s.%s",names(res),as.character(dfGr[1,vGrupo]))
+                            res
+                          }
+                        ),#AQUI VA ANOVA
+                        list(descAnova1F(dfTiempo,vNum,vGrupo)[,c("p.F","p.kw")])
+                        )#Aquí termina ANOVA
+                        )
+                    ))%>% mutate(Variable=traduccion[vNum],
+                                 Tiempo=levels(df[,TiempoFactor])) %>% select(Variable,Tiempo,everything())
+                  )
+  )
+  tablaRes
+}
 
 
 generaTablaBinariasPorTiempoGrupo=function(df,TiempoFactor,vGrupo,vBinarias,traduccion){
@@ -171,11 +204,11 @@ generaTablaBinariasPorTiempoGrupo=function(df,TiempoFactor,vGrupo,vBinarias,trad
 
 
 
-desc1vn=function(df,vNum,formato="%1.1f±%1.1f",formatoIntervalo="%1.1f-%1.1f"){
+desc1vn=function(df,vNum,formato="%1.2f±%1.2f",formatoIntervalo="%1.2f-%1.2f"){
   shapiro50="-"
   shapiro.p=1
-  resultado=data.frame("Fallo"=1,"n"=0,"media"=NA, "mediadt"="","mediaet"="","medianaRI"="","rango"="","gauss"="","out3SD"=NA,"out5SD"=NA,"shapiro"="","p25"=NA,"p50"=NA,"p75"=NA,"p.intra"="")
-  
+  resultado=data.frame("Fallo"=1,"n"=0,"media"=NA, "mediadt"="","mediaet"="","medianaRI"="","rango"="","gauss"="","out3SD"=NA,"out5SD"=NA,"shapiro"="","p25"=NA,"p50"=NA,"p75"=NA,"p.intra"="",ic1=NA,ic2=NA)
+
   try({
     datos=df[!is.na(df[,vNum]),vNum]
 
@@ -185,17 +218,19 @@ desc1vn=function(df,vNum,formato="%1.1f±%1.1f",formatoIntervalo="%1.1f-%1.1f"){
       set.seed(10)
       try({
         shapiro50.p=shapiro.test(sample(datos,min(40,length(datos))))$p.value
-      })
+      }, silent = TRUE)
       subdatos=datos
       if(length(datos)>4900) subdatos=subdatos[1:4900]
       try({
         shapiro.p=shapiro.test(subdatos)$p.value
         shapiro50=c("No","Ok")[1+as.integer(max(shapiro50.p,shapiro.p)>0.01)]
-      })
+      }, silent = TRUE)
     }
     zdatos=abs(scale(datos))
     zout3SD=sum(zdatos>3,na.rm=T)
     zout5SD=sum(zdatos>5,na.rm=T)
+
+    if(length(datos)>1) anchoIC=qt(.975,length(datos)-1) else anchoIC=NA
 
     resultado=df %>% mutate(numerica=df[,vNum]) %>% summarise(
       n=length(datos),
@@ -215,32 +250,76 @@ desc1vn=function(df,vNum,formato="%1.1f±%1.1f",formatoIntervalo="%1.1f-%1.1f"){
       shapiro=pvalores(shapiro.p),
       out3SD=zout3SD,
       out5SD=zout5SD,
-      p.intra=pvalores(2*pt(abs(media/et),n-1,lower.tail = F))
+      p.intra=pvalores(2*pt(abs(media/et),n-1,lower.tail = F)),
+      ic1=media-anchoIC*et,
+      ic2=media+anchoIC*et
     )
   }
-  })
+  }, silent = TRUE)
   resultado
 }
 
 
+
+descChi2=function(df,vCualiFila,vCualiColumna,margin=1,formato="%1.1f(%d)"){
+  if(!is.factor(df[,vCualiFila])) df[,vCualiFila]=factor(df[,vCualiFila])
+  frecuencias=xtabs( formula(sprintf("~ %s+%s",vCualiFila,vCualiColumna)),data=df)
+  porcentajes=100*prop.table(frecuencias,margin=margin)
+  resultado=data.frame(matrix(sprintf(formato,porcentajes,frecuencias),nrow = dim(frecuencias)[1],ncol= dim(frecuencias)[2]))
+  rownames(resultado)=levels(df[,vCualiFila])
+  names(resultado)=levels(df[,vCualiColumna])
+  resultado$p="";
+  try({
+    resultado$p[1]=pvalores(chisq.test(frecuencias)$p.value)
+  }, silent = TRUE)
+
+  resultado$Variable=vCualiFila
+  resultado$Valores=levels(df[,vCualiFila])
+  resultado %>% select(Variable,Valores,everything())
+  }
+
+
+
+
+
+generaTablaChi2PorGrupo=function(df,vGrupo,vCuali,traduccion,margin=margin){
+  names(traduccion)=vCuali
+  tablaRes=Reduce(rbind,
+                  lapply(vCuali,FUN=function(vCualiFila) descChi2(df, vCualiFila,vGrupo,margin) )
+  )
+  rownames(tablaRes)=NULL
+  tablaRes$Variable=traduccion[tablaRes$Variable]
+  tablaRes
+}
+
+
+
+
+
+
+
 descTtest=function(df,vNum,vFac,formato="%1.2f"){
-  res=data.frame("Error"=1,"p.t"="-","t"="-","dif"="-","ci_min"=NA,"ci_max"=NA,"ci95"="-")
+  res=data.frame("Error"=1,"p.t"="-","t"="-","dif"="-","ci_min"=NA,"ci_max"=NA,"ci95"="-","W"="-","p.w"="-")
   try({
     modelo=t.test(formula(sprintf("%s ~ %s",vNum,vFac)),data=df)
+    modelo.mw=wilcox.test(formula(sprintf("%s ~ %s",vNum,vFac)),data=df)
+
     res=data.frame("p.t"=pvalores(modelo$p.value),
-                   "t"=round(modelo$parameter,2),
-                   "dif"=modelo$estimate[2]-modelo$estimate[1],
-                   "ci_min"=modelo$conf.int[1],
-                   "ci_max"=modelo$conf.int[2],
-                   "ci95"=sprintf(sprintf("%s[%s,%s]",formato,formato,formato),modelo$estimate[1]-modelo$estimate[2],modelo$conf.int[1],modelo$conf.int[2])
+                   "t"=round(modelo$statistic,2),
+                   "dif"=-modelo$estimate[2]-modelo$estimate[1],
+                   "ci_min"=-modelo$conf.int[2],
+                   "ci_max"=-modelo$conf.int[1],
+                   "ci95"=sprintf(sprintf("%s[%s,%s]",formato,formato,formato),-modelo$estimate[1]+modelo$estimate[2],-modelo$conf.int[2],-modelo$conf.int[1]),
+                   "W"=round(modelo.mw$statistic,3),
+                   "p.w"=pvalores(modelo.mw$p.value)
     )
-  })
+  }, silent = TRUE)
   rownames(res)=NULL
   res
 }
 
 
-descAnova1F=function(df,vNum,vFac,formato="%1.2f"){
+descAnova1F=function(df,vNum,vFac,formato="%1.3f"){
   res=data.frame("Error"=1,"p.F"="-","F"="-","p.kw"="-","gauss"="-","shapiro"="-")
   shapiro50=shapiro.p=p.kw=NA
 
@@ -254,27 +333,27 @@ descAnova1F=function(df,vNum,vFac,formato="%1.2f"){
       set.seed(10)
       try({
         shapiro50.p=shapiro.test(sample(datos,min(40,length(datos))))$p.value
-      })
+      }, silent = TRUE)
       subdatos=datos
       if(length(datos)>4900) subdatos=subdatos[1:4900]
       try({
         shapiro.p=shapiro.test(subdatos)$p.value
         shapiro50=c("No","Ok")[1+as.integer(max(shapiro50.p,shapiro.p)>0.01)]
-      })
+      }, silent = TRUE)
     }
   }
   try ({
     kw=kruskal.test(laFormula, data = df)
     p.kw=kw$p.value
-  })
+  }, silent = TRUE)
 
   try({
     res=data.frame("p.F"=pvalores(1-pf(modelo$fstatistic[1],modelo$fstatistic[2],modelo$fstatistic[3])),
                    "F"=round(modelo$fstatistic[1],2), "p.kw"=pvalores(p.kw),"gauss"=shapiro50,"shapiro"=pvalores(shapiro.p))
-  })
-  
-  })
-  
+  }, silent = TRUE)
+
+  }, silent = TRUE)
+
   rownames(res)=NULL
   res
 }
@@ -310,7 +389,7 @@ descAnova1Fijo1Random=function(df,vCodigo,vNum,vFacFijo,vFacRandom){
       "p.inter"=rep(pvalores(analisis[["p-value"]][4]),nFilas)
     )
 
-  })
+  }, silent = TRUE)
   rownames(res)=NULL
   names(res)=paste(c("F","p","F","p","F","p"),c(vFacFijo,vFacFijo,vFacRandom,vFacRandom,"Interaccion","Interaccion"),sep=".")
   res=res %>% mutate(Variable=vNum,Tiempo=unique(df$Tiempo)) %>% select(Variable, Tiempo,everything())
@@ -326,12 +405,12 @@ descMWtest=function(df,vNum,VFac){
 
 
 
-lineaNumerica=function(df,vNum,vFac,descriptoresDesc=c("n","mediaet"),descriptoresInf=c("p.t","ci95")){
+lineaNumerica=function(df,vNum,vFac,descriptoresDesc=c("n","mediaet"),descriptoresInf=c("t","p.t","ci95", "W","p.w")){
   extra_t=data.frame("Vacio"=1)
   bloques1f=lapply(split(df,df[,c(vFac)]),FUN = function(df){desc1vn(df,vNum)[,descriptoresDesc]})
   try({
     extra_t=descTtest(df,vNum,VFac,"%1.3f")[descriptoresInf]
-  })
+  }, silent = TRUE)
   res=Reduce(cbind,list(Reduce(cbind,bloques1f),extra_t))
   cbind(Variable=vNum,res)
 }
@@ -357,7 +436,7 @@ desc2x2=function(df,vFila,vCol,fila=2 ){
                        RR=sprintf("%1.3f[%1.3f-%1.3f]",res.rr$estimate,res.rr$conf.int[1],res.rr$conf.int[2])
                        #OR=sprintf("%1.3f[%1.3f-%1.3f]",res.or$estimate,res.or$conf.int[1],res.or$conf.int[2])
     )
-  })
+  }, silent = TRUE)
   res=res %>% select(Variable,everything())
   rownames(res)=NULL
   res
